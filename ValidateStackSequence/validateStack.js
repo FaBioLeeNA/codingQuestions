b5751d77fd6f8b97a5196386d7feec11411c3da7
Fabian Yee <fabianyee952@gmail.com>
2021-02-25 15:18:17 -0800

    implemented summ of all odd subarrays

--- ValidateStackSequence/validateStack.js
+++ ValidateStackSequence/validateStack.js
@@ -3,7 +3,6 @@ const level = 'medium'
 
 // Given two sequences pushed and popped with distinct values, return true if and only if this could have been the result of a sequence of push and pop operations on an initially empty stack.
 
- 
 
 // Example 1:
 
--- uniqueNumberOfOccurence/uniqueOccurence.js
+++ uniqueNumberOfOccurence/uniqueOccurence.js
@@ -1,3 +1,5 @@
+const link = 'https://leetcode.com/problems/unique-number-of-occurrences/'
+
 // Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
 
 
